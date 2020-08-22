52090ca6baf62535586d24349c76292bca4b4649
bnl <b_n_l@mail.ru>
2020-08-23 01:18:54 +0300

    Forms

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.get('/', function(req, res) {
-    res.sendFile(  __dirname +'/index.html');
+    res.sendFile(__dirname +'/index.html');
+});
+
+app.get('/check', function(req, res) {
+    if (req.query.user === 'Администратор') {
+        res.sendFile(__dirname +'/check.html');
+    } else {
+        res.sendFile(__dirname +'/checkn.html');
+    }
 });
 
 app.listen(3000);
